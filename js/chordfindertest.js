d1115d25032b40fc46c609dc9a04c8e0b455361e
Malb7mm <ykmeak9g@gmail.com>
2023-10-08 12:32:08 +0900

    ディグリー対応

--- js/chordfindertest.js
+++ js/chordfindertest.js
@@ -1,14 +1,15 @@
-import {Chord, ChordName, ChordFinder} from "./chordfinder.js"
+import {Chord, ChordName, ChordFinder} from "./chordfinder.js?2"
 
 export function reflesh() {
-    var input = $("#chordinput").val();
-    var chordNames = ChordFinder.find(ChordFinder.parse(input));
+    var chordInput = $("#chordinput").val();
+    var keyInput = $("#keyinput").val();
+    var chordNames = ChordFinder.find(ChordFinder.parse(chordInput));
     
     $("#result").children().children().each((i, e) => {
         if (i > 0) e.remove();
     });
     for (var chordName of chordNames) {
-        $(`<tr><td>${chordName.priority}</td><td>${chordName.name}</td></tr>`).appendTo($("#result").children());
+        $(`<tr><td>${chordName.priority}</td><td>${chordName.name}</td><td>${ChordFinder.degree(chordName.name, keyInput)}</td></tr>`).appendTo($("#result").children());
     }
 }
 
